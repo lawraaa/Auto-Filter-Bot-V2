b2fd1d39203098307b4c6061fabe27a0984c9371
lawraaa <100027430+lawraaa@users.noreply.github.com>
2022-03-03 21:15:08 +0530

    Update script.py

--- script.py
+++ script.py
@@ -53,12 +53,12 @@ No need add each filter again!
 Bot will automatically search for your files and give links to that!
 
 
-<b>© @TroJanzHEX</b>"""
+@JamesbonddOO7
 
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @JamesbonddOO7
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
